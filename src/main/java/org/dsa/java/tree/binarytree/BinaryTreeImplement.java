2d9959aa702edc653eb8b2332e21cd6116a35481
Vishal Soni <50075856+vishalsoni008@users.noreply.github.com>
2023-03-17 11:13:47 +0530

    added pre order using iteration

--- src/main/java/org/dsa/java/tree/binarytree/BinaryTreeImplement.java
+++ src/main/java/org/dsa/java/tree/binarytree/BinaryTreeImplement.java
@@ -1,5 +1,7 @@
 package org.dsa.java.tree.binarytree;
 
+import java.util.Stack;
+
 public class BinaryTreeImplement {
     private TreeNode root;
 
@@ -29,13 +31,33 @@ public class BinaryTreeImplement {
 
     }
 
-    private void preOrderTraversal(TreeNode root){
+    private void preOrderTraversalUsingRecursion(TreeNode root){
         if (root == null){
             return;
         }
         System.out.print(root.data+" ");
-        preOrderTraversal(root.left);
-        preOrderTraversal(root.right);
+        preOrderTraversalUsingRecursion(root.left);
+        preOrderTraversalUsingRecursion(root.right);
+    }
+    private void preOrderUsingItration(){
+        if(root == null)
+            return;
+
+        Stack<TreeNode> st = new Stack<>();
+        st.push(root);
+
+        while (!st.empty()){
+            TreeNode temp = st.pop();
+
+            System.out.print(temp.data +" ");
+
+            if(temp.right != null){
+                st.push(temp.right);
+            }
+            if(temp.left != null){
+                st.push(temp.left);
+            }
+        }
     }
 
     public static void main(String[] args) {
@@ -43,7 +65,11 @@ public class BinaryTreeImplement {
         BinaryTreeImplement binaryTreeImplement = new BinaryTreeImplement();
 
         binaryTreeImplement.create();
-        binaryTreeImplement.preOrderTraversal(binaryTreeImplement.root);
+        System.out.println("presorder using recursion");
+        binaryTreeImplement.preOrderTraversalUsingRecursion(binaryTreeImplement.root);
+        System.out.println();
+        System.out.println("presorder using iteration");
+        binaryTreeImplement.preOrderUsingItration();
 
     }
 }
