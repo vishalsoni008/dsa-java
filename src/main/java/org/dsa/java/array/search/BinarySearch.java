3d989ce3dff0b9bc7051b63d8d5a69a5d46dbbb3
Vishal Soni <50075856+vishalsoni008@users.noreply.github.com>
2023-04-15 16:43:59 +0530

    added program of arrays

---
+++ src/main/java/org/dsa/java/array/search/BinarySearch.java
@@ -0,0 +1,14 @@
+package org.dsa.java.array.search;
+
+public class BinarySearch {
+
+//    int binarySearch(int arr[], int target){
+//        int l=0;
+//        int r = arr.length;
+//        int mi
+//    }
+
+    public static void main(String[] args) {
+        int arr[] = {1,5,3,6,43,7};
+    }
+}
