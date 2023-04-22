6498cd01c1b744a5ccf9114a46484cf77e84309e
Vishal Soni <50075856+vishalsoni008@users.noreply.github.com>
2023-04-22 13:14:24 +0530

    added Binary search program

--- src/main/java/org/dsa/java/array/search/BinarySearch.java
+++ src/main/java/org/dsa/java/array/search/BinarySearch.java
@@ -2,13 +2,35 @@ package org.dsa.java.array.search;
 
 public class BinarySearch {
 
-//    int binarySearch(int arr[], int target){
-//        int l=0;
-//        int r = arr.length;
-//        int mi
-//    }
+    public static int binarySearch(int arr[], int target) {
+        int left = 0;
+        int right = arr.length-1;
 
+        while (left<=right){
+            int mid = (left+right)/2;
+
+            if(arr[mid] == target){
+                return mid;
+            }
+            else if(arr[mid]>target){
+                right = mid-1;
+            }
+            else {
+                left = mid+1;
+            }
+        }
+        return -1;
+    }
     public static void main(String[] args) {
-        int arr[] = {1,5,3,6,43,7};
+        int arr[] = {1,2,3,4,5,6,7,8,9};
+
+        int result = binarySearch(arr, 8);
+
+        if(result == -1){
+            System.out.println("element is not present here");
+        }
+        else {
+            System.out.println("element is present in index of "+result);
+        }
     }
 }
